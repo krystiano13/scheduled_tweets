342827efab45cb9b7e5b0cfb3bb45e6f18f9284f
krystiano13 <crystianotv@gmail.com>
2024-04-04 13:59:39 +0200

    implement error handling for register form

--- app/controllers/concerns/register_controller.rb
+++ app/controllers/concerns/register_controller.rb
@@ -1,9 +1,19 @@
 class RegisterController < ApplicationController
-  def index()
+  def index
     @user = User.new
   end
 
-  def create()
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Account Created"
+    else
+      render :index
+    end
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "/about", to: "about#index"
-  get "/", to: "main#index"
+  root to: "main#index"
   get "/register", to: "register#index"
 
   post "/register", to: "register#create"
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
