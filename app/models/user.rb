ca6b8288809bbaaef2eae232455bed071e495128
krystiano13 <crystianotv@gmail.com>
2024-04-04 10:40:58 +0200

    create regex for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 # email:string
-# password: string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
 
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ , message: "Email is not valid" }
 end
