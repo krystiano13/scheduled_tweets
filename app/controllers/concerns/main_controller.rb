fe6f11312f91fb609da716fb181f575258364084
krystiano13 <crystianotv@gmail.com>
2024-04-05 13:52:30 +0200

    implement logging in after registering

--- app/controllers/concerns/main_controller.rb
+++ app/controllers/concerns/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
     def index()
         flash[:message] = "Hello User !"
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        end
     end
 end
--- app/controllers/concerns/register_controller.rb
+++ app/controllers/concerns/register_controller.rb
@@ -6,6 +6,7 @@ class RegisterController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Account Created"
     else
       render :index
