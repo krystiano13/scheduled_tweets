85cd9c5ff4e07702ed485c541fcd489aad995ee2
krystiano13 <crystianotv@gmail.com>
2024-04-05 14:09:01 +0200

    create view for logging in

--- app/controllers/concerns/session_controller.rb
+++ app/controllers/concerns/session_controller.rb
@@ -1,4 +1,11 @@
 class SessionController < ApplicationController
+  def index
+    @user = User.new
+  end
+
+  def create
+
+  end
   def delete
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged Out"
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/about", to: "about#index"
   get "/register", to: "register#index"
+  get "/login", to: "session#index"
 
   post "/register", to: "register#create"
+  post "/login", to: "session#create"
 
   delete "/logout", to: "session#delete"
 end
