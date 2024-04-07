ec2479b48259ca1335a5b920de162ac9122a93db
krystiano13 <crystianotv@gmail.com>
2024-04-07 21:04:10 +0200

    make user data visible on all views and controllers

--- app/controllers/concerns/main_controller.rb
+++ app/controllers/concerns/main_controller.rb
@@ -1,8 +1,5 @@
 class MainController < ApplicationController
     def index()
         flash[:message] = "Hello User !"
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
     end
 end
