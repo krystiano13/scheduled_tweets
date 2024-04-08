fe77d9aac482767864710855ffae52af9cec7788
krystiano13 <crystianotv@gmail.com>
2024-04-08 17:21:04 +0200

    create view for password-change form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
+
   def check_log_in
     if !session[:user_id]
       redirect_to login_path, error: "You must to be logged in first"
