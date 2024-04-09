424b4bf5909080ce9675882f21b1383edf3b8e89
krystiano13 <crystianotv@gmail.com>
2024-04-09 13:08:19 +0200

    implement detecting if email exists in database (for password reset feature)

--- app/controllers/concerns/password_reset_controller.rb
+++ app/controllers/concerns/password_reset_controller.rb
@@ -4,6 +4,14 @@ class PasswordResetController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
 
+    if @user.present?
+      #Send email
+      redirect_to root_path, notice: "Email for password reset was sent"
+    else
+      flash[:error] = "Account with this email address does not exist"
+      render :index
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   get "/register", to: "register#index"
   get "/login", to: "session#index"
   get "/password", to: "password#index"
-  get "/password/reset", to: "password_reset#index", as: :password_reset
+  get "password/reset", to: "password_reset#index"
 
   patch "/password", to: "password#update"
 
   post "/register", to: "register#create"
   post "/login", to: "session#create"
-  post "/password/reset", to: "password_reset#create"
+  post "password/reset", to: "password_reset#create"
 
   delete "/logout", to: "session#delete"
 end
